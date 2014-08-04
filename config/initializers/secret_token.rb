d212789a0dcc49e609e1f8ed2ce8be67e93ae358
Antonio F. Lancho <onioman@gmail.com>
2014-08-04 19:35:35 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArmarioApp::Application.config.secret_key_base = '3c3ac26acdff5abdfda72139ce30fe02ce73554e714339c3f44340c3612fe861ee7c629f035b063cad92ab7356b6c04724c9c106e7922801725d4a46ff1ceb1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArmarioApp::Application.config.session_store :cookie_store, key: '_armario_app_session'
