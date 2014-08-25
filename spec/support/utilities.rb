e0f4cf07d79dc3b4bf1a6e3acfc38faf69c9585c
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-08-25 14:58:41 +0900

    Implemeting user models.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,1 @@
 include ApplicationHelper
-
-#def full_title(page_title)
-#    base_title = "MiArmario"
-#    if page_title.empty?
-#        base_title
-#    else
-#        "#{base_title} | #{page_title}"
-#    end
-#end
