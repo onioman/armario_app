4fc3e851991dbd2e97c82efa68b460eb4a75dd16
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-09-03 16:40:38 +0900

    Addn sign in feature.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Authentication" do
     subject { page }
 
     describe "singin page" do
-        before { visit singin_path }
+        before { visit signin_path }
 
         it { should have_content('Sign in') }
         it { should have_title('Sign in') }
