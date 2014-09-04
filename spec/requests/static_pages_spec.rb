fb2cff46b1e660e8dfdc8598ecc8c27c87da0246
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-09-04 15:01:29 +0900

    Some more decoupling.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "StaticPages" do
         subject { page }
 
         shared_examples_for "all static pages" do
-            it { should have_selector('h1', text:heading) }
+            it { should have_header(heading) }
             it { should have_title(full_title(page_title)) }
         end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,15 @@ RSpec::Matchers.define :have_error_message do |message|
         expect(page).to have_selector('div.alert.alert-error', text: message);
     end
 end
+
+RSpec::Matchers.define :have_welcome_message do |message|
+    match do |page|
+        expect(page).to have_selector('div.alert.alert-success', text: message);
+    end
+end
+
+RSpec::Matchers.define :have_header do |message|
+    match do |page|
+        expect(page).to have_selector('h1', text: message);
+    end
+end
