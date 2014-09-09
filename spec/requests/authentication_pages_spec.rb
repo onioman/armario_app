c0b25ab8a6a7159e486f0f0e90e580dd17dcc7e4
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-09-09 14:19:08 +0900

    User index.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,8 +30,9 @@ describe "Authentication" do
             before { valid_signin(user) }
 
             it { should have_title(user.name) }
-            it { should have_link('Profile',     href: user_path(user))}
-            it { should have_link('Settings',    href: edit_user_path(user))}
+            it { should have_link('Users',       href: users_path) }
+            it { should have_link('Profile',     href: user_path(user)) }
+            it { should have_link('Settings',    href: edit_user_path(user)) }
             it { should have_link('Sign out',    href: signout_path) }
             it { should_not have_link('Sing in', href: signin_path) }
 
@@ -72,6 +73,11 @@ describe "Authentication" do
             describe "sugmitting to the update action" do
                 before { patch user_path(user) }
                 specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "visiting the user index" do
+                before { visit users_path }
+                it { should have_title('Sign in') }
             end
         end
 
