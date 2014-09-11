714d1b308b2be89a330435ff1578d34050f68dbd
Antonio F. Lancho <aflancho@tranzas.co.jp>
2014-09-11 14:19:05 +0900

    Delete users.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,11 @@ FactoryGirl.define do
     factory :user do
         sequence(:name)     { |n| "Person #{n}" }
         sequence(:email)    { |n| "person_#{n}@example.com" }
-        password    "foobar"
+        password "foobar"
         password_confirmation "foobar"
+
+        factory :admin do
+            admin true
+        end
     end
 end
